dcda7bb8329b101987b7aba99ae040f41ac31d27
googlemeoften <1919435320@qq.com>
2016-03-27 14:34:48 +0800

    单例模式

--- src/main/java/cn/edu/learn/interview/design/single/SingleTon.java
+++ src/main/java/cn/edu/learn/interview/design/single/SingleTon.java
@@ -1,6 +1,4 @@
-package cn.edu.learn.interview.design;
-
-import java.util.concurrent.locks.ReentrantLock;
+package cn.edu.learn.interview.design.single;
 
 /**
  * @description: 单例模式
@@ -12,7 +10,7 @@ public class SingleTon {
 
     private SingleTon() {}
 
-    public SingleTon getInstance() {
+    public static SingleTon getInstance() {
         if (singleTon == null)
             synchronized (SingleTon.class) {
                 if (singleTon == null) {
